07ecf19e585bc589b03cd6591a7a18f355d3f1c0
Rafaela Alexandre <rafaelaalexandre619@gmail.com>
2024-04-12 15:23:04 -0300

    registro forms

---
+++ minimalistApp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import UserCreationForm
+from .models import Post
+from django.contri.auth.forms import AuthenticationForm
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
