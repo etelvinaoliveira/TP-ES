097f142b3343bafcba83d270bbce0a2d12946482
LeoOMaia <leonardooliveiramaia1@gmail.com>
2024-04-14 11:57:24 -0300

    incluindo formulario padrao do django

--- minimalistApp/forms.py
+++ minimalistApp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Post
-from django.contri.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -11,3 +11,6 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+class LoginForm(AuthenticationForm):
+    pass
