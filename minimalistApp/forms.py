f7cc41c0225f240073a92e71c2f84ec3aa93c17a
LeoOMaia <leonardooliveiramaia1@gmail.com>
2024-04-14 12:25:31 -0300

    adicionando html inicial e view para metodo GET do formulario de login

--- minimalistApp/forms.py
+++ minimalistApp/forms.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 from .models import Post
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    email = forms.EmailField()
         
 class LoginForm(AuthenticationForm):
     pass
