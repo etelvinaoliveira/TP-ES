e586b45e3dae55af0db3eb70a67686ed38f6dc6b
LeoOMaia <leonardooliveiramaia1@gmail.com>
2024-04-14 11:59:41 -0300

    refatoracao de linhas em forms

--- minimalistApp/forms.py
+++ minimalistApp/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Post
-from django.contrib.auth.forms import AuthenticationForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
