bef2304f65cd9fafd4fe41b8f07bec850d2afa93
Etelvina <etelvina.oliveira2003@gmail.com>
2024-04-16 08:44:16 -0300

    URL e View para lista de publicações

--- minimalistApp/urls.py
+++ minimalistApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # path(),
+  path('', views.postList, name='postList'),
   path('register/', views.register, name='register'),
   path('post/<int:pk>/', views.postDetail, name='postDetail'),
 ]
